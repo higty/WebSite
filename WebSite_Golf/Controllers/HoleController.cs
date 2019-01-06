4d1ff70bf89edee397dca24ca790bc3b5e1fb1f3
higty <higuchihiguchihiguchi@hotmail.com>
2019-01-06 16:18:44 +0900

    Add WebSite_Golf.

---
+++ WebSite_Golf/Controllers/HoleController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite.Controllers
+{
+    public class HoleController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Root()
+        {
+            return View();
+        }
+        [HttpGet("/Masters/Hole/{number}")]
+        public IActionResult Hole(Int32 number)
+        {
+            if (number == 2 || number == 7)
+            {
+                return View("Hole" + number);
+            }
+            return View("UnderConstruction");
+        }
+    }
+}
